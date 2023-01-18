a1f102386999f61224c0394207d68088420d699f
IsabelleTancred <isabelleetancred@gmail.com>
2023-01-18 14:31:11 +0100

    did some clean-up on code

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const laptopNameElement = document.getElementById("laptopName");
 const laptopPriceElement = document.getElementById("laptopPrice");
 const laptopImageElement = document.getElementById("laptopImage");
 const laptopDescriptionElement = document.getElementById("laptopDescription");
-const imageBaseUrl = "https://hickory-quilled-actress.glitch.me/"; 
+const imageBaseUrl = "https://hickory-quilled-actress.glitch.me/";
 const featureListElement = document.getElementById("featureList");
 const balanceElement = document.getElementById("balance");
 const debtElement = document.getElementById("debt");
@@ -16,88 +16,90 @@ const workButtonElement = document.getElementById("work");
 const transferButtonElement = document.getElementById("transferBank");
 const payLoanButtonElement = document.getElementById("payLoan");
 
-
-
 let laptops = [];
 let features = [];
-let firstLaptop =[];
+let firstLaptop = [];
 
 // function to convert a number to be displayed as SEK
-function toSek (num){
-    return (new Intl.NumberFormat('se-DE',{ style: 'currency', currency: 'SEK' }).format(num));
+function toSek(num) {
+  return new Intl.NumberFormat("se-DE", {
+    style: "currency",
+    currency: "SEK",
+  }).format(num);
 }
 //function for changing innerHTML to a new value
-function display(element, value){
-  return element.innerHTML = (value);
+function display(element, value) {
+  return (element.innerHTML = value);
 }
 
 //function too add features to a list and display them. First clear the list if it already has items in it.
-const addFeatures = (features)=>{
-  while(featureListElement.hasChildNodes())
-    {
-        featureListElement.removeChild(featureListElement.firstChild)
-    }
-    features.forEach((item)=>{
-        let dt = document.createElement("dt");
-        dt.innerText = item;
-        featureListElement.appendChild(dt);
-      })
-}
+const addFeatures = (features) => {
+  while (featureListElement.hasChildNodes()) {
+    featureListElement.removeChild(featureListElement.firstChild);
+  }
+  features.forEach((item) => {
+    let dt = document.createElement("dt");
+    dt.innerText = item;
+    featureListElement.appendChild(dt);
+  });
+};
 
 //fetch API
 fetch("https://hickory-quilled-actress.glitch.me/computers")
-    .then(response => response.json())
-    .then(data => laptops = data)
-    .then(laptops => addLaptopsToMenu(laptops));
+  .then((response) => response.json())
+  .then((data) => (laptops = data))
+  .then((laptops) => addLaptopsToMenu(laptops));
 
-//function to add laptops to menu select, start-page shows the first laptop in select-menu    
+//function to add laptops to menu select, start-page shows the first laptop in select-menu
 const addLaptopsToMenu = (laptops) => {
-    let firstLaptop = laptops[0];
-    laptops.forEach(x => addLaptopToMenu(x))
-    display(laptopNameElement, firstLaptop.title);
-    display(laptopPriceElement, toSek(firstLaptop.price));
-    display(laptopDescriptionElement, firstLaptop.description);
-    laptopImageElement.setAttribute ('src', (imageBaseUrl + firstLaptop.image));
-    features = firstLaptop.specs;
-    addFeatures (features);
-}  
+  let firstLaptop = laptops[0];
+  laptops.forEach((x) => addLaptopToMenu(x));
+  display(laptopNameElement, firstLaptop.title);
+  display(laptopPriceElement, toSek(firstLaptop.price));
+  display(laptopDescriptionElement, firstLaptop.description);
+  laptopImageElement.setAttribute("src", imageBaseUrl + firstLaptop.image);
+  features = firstLaptop.specs;
+  addFeatures(features);
+};
 
 //function to add each laptop from API as an option in the select menu
 const addLaptopToMenu = (laptop) => {
-    const laptopElement = document.createElement ("option");
-    laptopElement.value = laptop.id;
-    laptopElement.appendChild(document.createTextNode(laptop.title));
-    laptopMenuElement.appendChild(laptopElement);
+  const laptopElement = document.createElement("option");
+  laptopElement.value = laptop.id;
+  laptopElement.appendChild(document.createTextNode(laptop.title));
+  laptopMenuElement.appendChild(laptopElement);
+};
 
-}
-
-//function which handles changes in the select menu. Also catches error in image display. 
-const handleLaptopMenuChange = e => {
-    const selectedLaptop = laptops [e.target.selectedIndex];
-    laptopNameElement.textContent = selectedLaptop.title;
-    laptopPriceElement.textContent = toSek(selectedLaptop.price);
-    laptopDescriptionElement.textContent = selectedLaptop.description;
-    laptopImageElement.setAttribute('src', (imageBaseUrl + selectedLaptop.image));
-    laptopImageElement.onerror = function() {changeImage()};
-    function changeImage() {
-      laptopImageElement.setAttribute('src', (imageBaseUrl + changeImgFormat(selectedLaptop.image)));
-    }
-    features = selectedLaptop.specs;
-    addFeatures(features);
-}
-
+//function which handles changes in the select menu. Also catches error in image display.
+const handleLaptopMenuChange = (e) => {
+  const selectedLaptop = laptops[e.target.selectedIndex];
+  laptopNameElement.textContent = selectedLaptop.title;
+  laptopPriceElement.textContent = toSek(selectedLaptop.price);
+  laptopDescriptionElement.textContent = selectedLaptop.description;
+  laptopImageElement.setAttribute("src", imageBaseUrl + selectedLaptop.image);
+  laptopImageElement.onerror = function () {
+    changeImage();
+  };
+  function changeImage() {
+    laptopImageElement.setAttribute(
+      "src",
+      imageBaseUrl + changeImgFormat(selectedLaptop.image)
+    );
+  }
+  features = selectedLaptop.specs;
+  addFeatures(features);
+};
 
 laptopMenuElement.addEventListener("click", handleLaptopMenuChange);
 
 // function to change img format from jpg to png, or from png to jpg
-function changeImgFormat(str){
+function changeImgFormat(str) {
   let str1 = "jpg";
   let str2 = "png";
-  if (str.includes(str1)){
-    return (str.replace(/jpg/i, str2))
-  }
-  else if (str.includes(str2)){
-    return (str.replace(/png/i, str1))
+  if (str.includes(str1)) {
+    return str.replace(/jpg/i, str2);
+  } else if (str.includes(str2)) {
+    return str.replace(/png/i, str1);
   }
 }
 
@@ -106,7 +108,7 @@ function changeImgFormat(str){
 let balance = 0;
 let debt = 0;
 let salary = 0;
- 
+
 //sets initial values to be displayed as SEK
 display(balanceElement, toSek(balance));
 display(debtElement, toSek(debt));
@@ -127,16 +129,14 @@ function hideOrShowLoanElements(debt) {
     debtDivElement.style.display = "none";
     payLoanButtonElement.style.display = "none";
     debtInfoElement.style.display = "none";
-  }
-  else {
+  } else {
     debtDivElement.style.display = "block";
     payLoanButtonElement.style.display = "block";
     debtInfoElement.style.display = "block";
   }
 }
 
-
-//function update and display value when deposit 
+//function update and display value when deposit
 const deposit = (amount) => {
   balance += amount;
   display(balanceElement, toSek(balance));
@@ -151,10 +151,8 @@ const withdraw = (amount) => {
 };
 const clearSalary = (amount) => {
   salary -= amount;
-  display(salaryElement,toSek(salary));
+  display(salaryElement, toSek(salary));
 };
-
-
 
 const transfer = (amount) => {
   let ten = amount / 10;
@@ -179,7 +177,7 @@ const getDebt = (amount) => {
 };
 const loanPayment = (amount) => {
   debt -= amount;
-  display(debtElement,toSek(debt))
+  display(debtElement, toSek(debt));
   hideOrShowLoanElements(debt);
 };
 
@@ -190,17 +188,16 @@ const getLoan = (input) => {
     getDebt(input);
   } else if (input > 0) {
     alert("You can only have one loan");
-  } else if ((input > balance * 2)){
+  } else if (input > balance * 2) {
     alert("You don't have enough money");
-  }
-  else{
+  } else {
     alert("Wrong input, use digits");
   }
-} 
+};
 
 //function to handle logic when repayLoan have been pressed
- const repayLoan=(salary)=>{
-  let restSalary=(salary-debt);
+const repayLoan = (salary) => {
+  let restSalary = salary - debt;
   if (salary > debt) {
     loanPayment(debt);
     deposit(restSalary);
@@ -211,13 +208,16 @@ const getLoan = (input) => {
 };
 //function to check if purchase is possible, and if so handles purchase
 const buyLaptop = () => {
-  if (balance >=fromSek(laptopPriceElement.innerHTML)) {
+  if (balance >= fromSek(laptopPriceElement.innerHTML)) {
     withdraw(fromSek(laptopPriceElement.innerHTML));
-    alert("Congratulation! You are now the owner of the " + laptopNameElement.innerHTML );
+    alert(
+      "Congratulation! You are now the owner of the " +
+        laptopNameElement.innerHTML
+    );
   } else {
-   alert("You can't afford this computer");
+    alert("You can't afford this computer");
   }
-}
+};
 /*Below are functions to handle button inputs, should probably an own JS-file as well*/
 
 //function to handle that loan button has been pressed
@@ -242,14 +242,13 @@ transferButtonElement.addEventListener("click", handleTransferButton);
 //function to handle when repay loan has been pressed
 const handlePayLoanButton = (e) => {
   repayLoan(salary);
-}
-
+};
 
 payLoanButtonElement.addEventListener("click", handlePayLoanButton);
 
-function fromSek (str){
+function fromSek(str) {
   const res = str.replace(/\D/g, "");
-  return (res/100);
+  return res / 100;
 }
 
 const handlePayButton = (e) => {
@@ -257,4 +256,4 @@ const handlePayButton = (e) => {
 };
 payLaptopElement.addEventListener("click", handlePayButton);
 
-hideOrShowLoanElements (debt);
+hideOrShowLoanElements(debt);
